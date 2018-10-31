eb28449055676619c41108b986549eb281856f3d
John Wood <johnnathanwood@gmail.com>
2018-10-31 09:01:51 -0500

    added new way to handle number 4

--- src/js/main.js
+++ src/js/main.js
@@ -16,12 +16,43 @@ console.log(fave_demo)
 // expected result:
 // `[3, 5, 7, 9, 11, 13]`
 
+let nums = [1, 2, 3, 4, "5", 6, "7"]
+
+let numsArray = nums.map((a, b) => {
+
+    return parseInt(a) + parseInt(b)
+  })
+numsArray
+
 
 // 3) Capitalize 'the', insert a comma after 'teacher' and output "Yoda says, "The greatest teacher, failure is"
 
-// `let yoda_quote = ["the", "greatest", "teacher", "failure", "is"]`
+let yoda_quote = ["the", "greatest", "teacher", "failure", "is"]
+yoda_quote[0]="The"
+yoda_quote[2]="teacher,"
+yoda_quote.unshift("Yoda says,")
+let yodaAnswer = yoda_quote.reduce((a,b) => `${a} ${b}`)
+yodaAnswer
+
+let yoda_quote1 = ["the", "greatest", "teacher", "failure", "is"]
+
+let assembledQuote = yoda_quote.map(word => {
+  if(word === "the") word = "The"
+  if(word === "teacher") word = "teacher,"
+  return word
+})
+
+console.log(`Yoda says, "${assembledQuote.join(" ")}"`)
 
 // 4)  * Sort the following numbers in descending order
 //    * Remove any integers greater than 19.
 //    * Multiply each remaining number by 1.5 and then substract 1.
 //    * Then output (either in the DOM or the console) the sum of all the resulting numbers.
+
+const integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8]
+let integerAnswer = integers.sort((a, b) => b - a).filter(a => a.valueOf() < 19).map(a => a * 1.5 - 1).reduce((a, b) => a + b)
+integers
+integerAnswer
+
+
+
